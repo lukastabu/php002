87b3a4ebf2de388526cb2b258b48d0a80c8104e7
Lukas <lukas.tabulevicius@gmail.com>
2022-05-05 12:27:05 +0300

    initial

---
+++ labas.php
@@ -0,0 +1,37 @@
+<h1>Labdien</h1>
+
+<?php
+echo 'labas su php';
+
+?>
+
+<h2>Labdien</h2>
+
+<?php
+echo 2*2 ;
+echo "<br>";
+
+$kintamasis='kintamss' ;
+echo $kintamasis;
+echo ($kintamasis);
+print($kintamasis);
+print_r ($kintamasis);
+
+echo "<div>", $kintamasis, "</div>";
+
+// taip atrodo komentaras
+/* arba 
+taip
+*/
+
+$a = 2;
+echo $a++ * ++$a, "<br>";
+$b = 2;
+echo ++$b * $b++;
+
+echo "<br>";
+echo 'vienas'. "du";
+// echo 'vienas' + "du"; jau neveiks
+
+echo "\n $kintamasis \n";
+echo '\n $kintamasis \n';
