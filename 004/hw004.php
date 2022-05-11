edacf56240b87754502947fd58de0564e414b8aa
Lukas <lukas.tabulevicius@gmail.com>
2022-05-11 07:25:29 +0300

    sestas, septinas, desimtas

--- 004/hw004.php
+++ 004/hw004.php
@@ -53,19 +53,56 @@ $hKiekis = 0;
 
 do {
     if (!(--$saugiklis)) break; // su kiekviena iteracija mazina saugikli, o kei jis tampa 0 (false), ! ji padaro true ir isijungia break
-    $moneta = rand(0, 1) ? 'H' : 'S';
+    
+    $moneta = (!rand(0, 1)) ? 'H' : 'S';
     if ($moneta=="H") {
         $hKiekis++;
     }
-    $rezultatai = $rezultatai.$moneta;
-    echo $rezultatai;
+    // if (rand(0, 1)) {
+    //     $moneta = 'H';
+    //     $hKiekis++;
+    // } else {
+    // $moneta = 'S';
+    //  }
+    // if ($moneta=="H") {
+    //     $hKiekis++;
+    // }
+    $rezultatai .= $moneta;
+    echo $moneta;
     
-// } while ($moneta != 'H'); // sustos kai iskris H
-} while  ($hKiekis <= 3); // sustos kai H iskris tris kartus
-// } while (!strpos($rezultatai, 'HHH)); // sustos kai H iskris tris kartus is eiles
+} while ($moneta != 'H'); // sustos kai iskris H
+// } while  ($hKiekis < 3); // sustos kai H iskris tris kartus
+// } while (!strpos($rezultatai, 'HHH')); // sustos kai H iskris tris kartus is eiles
 
-// echo nl2br ("\n xxxxxxxxxxxxx \n");
-// echo nl2br ("\n Septintas uzdavinys \n");
+echo nl2br ("\n xxxxxxxxxxxxx \n");
+echo nl2br ("\n Septintas uzdavinys \n");
+
+$petras = 0;
+$kazys = 0;
+$laimetojas = '';
+
+do {
+    $petras += rand(10, 20);
+    $kazys += rand(5, 25);
+    if ($petras > 221) {
+        $laimetojas = 'Petras';
+    }
+    elseif ($kazys > 221) {
+        $laimetojas = 'Kazys';
+    }
+} while ($laimetojas=='');
+if (($petras > 221) && ($kazys > 221)) {
+    echo "Petras: $petras, Kazys:$kazys, partija laimejo abu 🤷‍♂️";
+} else echo "Petras: $petras, Kazys:$kazys, partija laimejo $laimetojas";
+
+// if ($petras > 221) {
+//     echo "Petras: $petras, Kazys:$kazys, partija laimejo Petras";
+// }
+// elseif ($kazys < 221) {
+//     echo "Petras: $petras, Kazys:$kazys, partija laimejo Kazys";
+// }
+
+
 
 // echo nl2br ("\n xxxxxxxxxxxxx \n");
 // echo nl2br ("\n Astuntas uzdavinys \n");
@@ -73,8 +110,30 @@ do {
 // echo nl2br ("\n xxxxxxxxxxxxx \n");
 // echo nl2br ("\n Devintas uzdavinys \n");
 
-// echo nl2br ("\n xxxxxxxxxxxxx \n");
-// echo nl2br ("\n Desimtas uzdavinys \n");
+echo nl2br ("\n xxxxxxxxxxxxx \n");
+echo nl2br ("\n Desimtas uzdavinys \n");
+
+$visoMazuSmugiu = 0;
+for ($i=0; $i < 5; $i++) { 
+    $vienoViniesIkalta = 0;
+    while ($vienoViniesIkalta < 85) {
+        $vienoViniesIkalta += rand(5, 20);
+        $visoMazuSmugiu++;
+    }
+}
+echo "reikes $visoMazuSmugiu-iu mazu smugiu";
+echo '<br>';
+$visoDideliuSmugiu = 0;
+for ($i=0; $i < 5; $i++) { 
+    $vienoViniesIkalta = 0;
+    while ($vienoViniesIkalta < 85) {
+        $vienoViniesIkalta += (rand(20, 30)*rand(0, 1));
+        $visoDideliuSmugiu++;
+    }
+}
+echo "reikes $visoDideliuSmugiu-iu dideliu smugiu";
+
+
 
 // echo nl2br ("\n xxxxxxxxxxxxx \n");
 // echo nl2br ("\n Vienuoliktas uzdavinys \n");
