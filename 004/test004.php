4b4478db22c0eb08a689de95713117ab6939064a
Lukas <lukas.tabulevicius@gmail.com>
2022-05-11 09:17:06 +0300

    solved

--- 004/test004.php
+++ 004/test004.php
@@ -7,6 +7,7 @@ $ziurkes = 0;
 do {
     $ziurkes += rand(3,5);
     $apibegimai++;
+    // echo $apibegimai, $ziurkes; Tarpinis pasitikrinimas ar veikia gerai
 }
 while ($ziurkes < 20);
-echo "apibegimai: $apibegimai <br> pagauta ziurkiu: $ziurkes";
+echo "Apibėgimai aplink namą: $apibegimai; <br> Pagauta žiurkių: $ziurkes;";
