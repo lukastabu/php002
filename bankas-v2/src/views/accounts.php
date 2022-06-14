ccda338535b59ab2647276213356f778a47445f3
Lukas <lukas.tabulevicius@gmail.com>
2022-06-14 15:46:04 +0300

    cleaned

--- bankas-v2/src/views/accounts.php
+++ bankas-v2/src/views/accounts.php
@@ -2,37 +2,7 @@
 
 require __DIR__ . '/top.php';
 
-// function konvertuok($user) {
-                
-//     $ch = curl_init();
-
-//     curl_setopt($ch, CURLOPT_URL, 'https://www.currency-api.com/base=EUR&date=2022-06-14&symbols=USD');
-
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-//     $output = curl_exec($ch); // siuncia, laukia, gauna
-
-//     curl_close($ch);     
-
-//     $output = json_decode($output);
-//     // echo $user;
-//     print_r($output->rates['USD']);
-//     // $_SESSION['dist'] = $output->distance;
-//     // return ($output->rates['UAH']*$user);
-//     die;
-//     }
-
-//     konvertuok(5);
-
-
-// function currencyConverter($amount) {
-// 	$get = file_get_contents("https://finance.google.com/finance/converter?a=1&from=eur&to=uah");
-// 	$get = explode("<span class=bld>",$get);
-// 	$get = explode("</span>",$get[1]);
-// 	$converted_currency = preg_replace("/[^0-9\.]/", null, $get[0]);
-// 	return $converted_currency;
-// }
-
+//https://stackoverflow.com/questions/50921997/how-to-convert-currency-using-google-currency-api-in-php
 function currency_convert($amount){
     $url = 'https://api.exchangerate-api.com/v4/latest/EUR';
     $json = file_get_contents($url);
